bd1d807fa1a94932a4c5adc487d5a516183d9aad
masochnik <zarypovd@yandex.ru>
2026-05-07 23:01:03 +0700

    prch

---
+++ src/logica/prch.js
@@ -0,0 +1,20 @@
+const prch = {
+  randomNum: function(min, max) {
+    const q = Math.random() * (max - min) + min;
+    return Math.round(q);
+  },
+  Num1_10: function() {
+    const q = Math.random() * 10 + 1;
+    return Math.round(q);
+  },
+  Num1_100: function() {
+    const q = Math.random() * (1 - 100) + 1;
+    return Math.round(q);
+  },
+  Num1_1000: function() {
+    const q = Math.random() * (1 - 1000) + 1;
+    return Math.round(q);
+  },
+};
+console.log(prch.Num1_10());
+if (typeof module !== "undefined") module.exports = prch;
